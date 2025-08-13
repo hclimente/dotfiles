bce5724c0de9385eac100af0dfb4bead4d0a534c
Hector Climente-Gonzalez <hclimente@gmail.com>
2025-08-13 20:57:28 +0100

    chore: clean environments folder

--- .dotfiles-cfg/env/bio3.sh
+++
@@ -1,3 +0,0 @@
-export SHORT_Q='urtgen_24hrs'
-export LONG_Q='urtgen_5days'
-export NXF_TEMP='/home/gallia/scratch/f051281'
--- .shell_config/80_host_logic.sh
+++ .shell_config/80_host_logic.sh
@@ -7,12 +7,6 @@ if grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/slurm_hosts"; then
     HOSTTYPE='cluster'
 elif grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/server_hosts"; then
     HOSTTYPE='server'
-elif grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/sge_hosts"; then
-    source "$DOTFILES_CFG/env/sge.sh"
-    HOSTTYPE='cluster'
-elif grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/gpu_hosts"; then
-    source "$DOTFILES_CFG/env/gpu.sh"
-    HOSTTYPE='server'
 elif grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/laptop_hosts"; then
     if which java > /dev/null; then
         export JAVA_HOME=$(/usr/libexec/java_home)
@@ -23,18 +17,3 @@ else
     HOSTTYPE='unknown'
 fi
 export HOSTTYPE
-
-# Determine HOSTLOC based on hostname lists
-if grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/mines_hosts"; then
-    source "$DOTFILES_CFG/env/mines.sh"
-    HOSTLOC='mines'
-elif grep --quiet "$HOSTNAME" "$DOTFILES_CFG/env/kyodai_hosts"; then
-    source "$DOTFILES_CFG/env/kyodai.sh"
-    HOSTLOC='kyodai'
-elif [[ "$HOSTNAME" == "master01" ]]; then
-    source "$DOTFILES_CFG/env/bio3.sh"
-    HOSTLOC='bio3'
-else
-    HOSTLOC='unknown'
-fi
-export HOSTLOC

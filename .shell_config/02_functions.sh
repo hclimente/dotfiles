43355acc015973d0d212309bebb3b88d964c4bc4
Hector Climente-Gonzalez <hclimente@gmail.com>
2025-08-13 21:29:41 +0100

    fix: make alias_if_exists more portable

--- .shell_config/02_functions.sh
+++ .shell_config/02_functions.sh
@@ -1,6 +1,8 @@
-# Create an alias only if the target command exists
 alias_if_exists() {
-    if command -v $2 >/dev/null; then
-        alias $1=$2
+    # Check if the target command (first part of $2) exists
+    # We use awk to extract only the command name before any spaces
+    local target_command=$(echo "$2" | awk '{print $1}')
+    if command -v "$target_command" >/dev/null; then
+        alias "$1=$2" # Quote the entire alias assignment
     fi
 }

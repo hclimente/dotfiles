e2e43809417ae9126863039929f044b110e4ff78
Hector Climente-Gonzalez <hclimente@gmail.com>
2025-09-05 00:34:08 +0100

    feat: handle vscode nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,18 @@
-require 'plugins'
-require 'colorscheme'
-require 'keymaps'
-require 'lsp'
-require 'options'
+if vim.g.vscode then
+    -- VSCode extension
+else
+    -- ordinary Neovim
+    require 'plugins'
+    require 'colorscheme'
+    require 'keymaps'
+    require 'lsp'
+    require 'options'
 
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
+    -- set termguicolors to enable highlight groups
+    vim.opt.termguicolors = true
 
-require("nvim-tree").setup()
+    require("nvim-tree").setup()
+end
